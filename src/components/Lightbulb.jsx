b0eb325eed0220dcfc0b02feb4505511d42811ed
Susanne Ekenheim <susanne.ekenheim@gmail.com>
2023-09-25 20:02:59 +0200

    Anna and me working together

--- src/components/Lightbulb.jsx
+++ src/components/Lightbulb.jsx
@@ -1,13 +1,19 @@
+import React, { useState } from "react";
+
 export const Lightbulb = () => {
   // Initialize the state for on/off
 
+    const [on, setOn] = useState(false);
+
   const toggleLight = () => {
     // Toggle the on/off state
+    
+    setOn(!on) 
     console.log("Clicked");
   };
   return (
     <div className="lightbulb-container">
-      <div className="lightbulb" onClick={toggleLight}>
+      <div className={`lightbulb ${on ? 'on' : 'off'}`} onClick={toggleLight}>
         <div className="bulb"></div>
       </div>
     </div>
